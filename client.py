5ce92507cb8b79102ebe0990d8109e1584a07177
Andhyy <adam.tomala@seznam.cz>
2026-04-01 17:52:44 +0200

    Server Ping

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import flet as ft
+import requests
 
 # --- COLORS ---
 DARK_GREY = "#121212"
@@ -7,6 +8,8 @@ RED_MAGENTA = "#FF0055"
 NEON_GREEN = "#00FF66"
 WHITE = "#FFFFFF"
 RED = "#FF0000"
+# --- ENDPOINT ---
+SERVER_URL = "https://4e62-178-209-155-47.ngrok-free.app/login"
 
 def main(page: ft.Page):
     page.title = "Chat Client"
@@ -19,21 +22,49 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
+    # Input Field
+    name_input = ft.TextField(
+        label="Enter your name",
+        color=WHITE,
+        border_color=RED_MAGENTA,
+        width=300
+    )
+
     # --- On Button Click ---
     def switch_ui(e):
+        # Get the name from input
+        user_name = name_input.value
+        if not user_name:
+            user_name = "Unknown User"
 
         # screen clear
         page.controls.clear()
-        
-        # --- UI after "Login" ---
+        page.add(ft.ProgressRing(color=NEON_GREEN))
+        page.update()
+
+        # Request to my server
+        try:
+            response = requests.post(SERVER_URL, json={"name": user_name})
+            data = response.json()
+            
+            # Grab whoami
+            server_user = data.get("server_user", "Unknown Server")
+            result_text = f"Hello World from {server_user}"
+            text_color = NEON_GREEN
+
+        except Exception as ex:
+            # Down handle
+            result_text = f"Connection failed! Is ngrok running?"
+            text_color = RED
+
+        # Draw result
+        page.controls.clear()
         hello_box = ft.Container(
-            content=ft.Text("Hello World", size=40, color=NEON_GREEN),
-            border=ft.border.all(3, RED_MAGENTA), 
-            padding=30,                           
-            border_radius=10                      
+            content=ft.Text(result_text, size=40, color=text_color),
+            border=ft.border.all(3, RED_MAGENTA),
+            padding=30,
+            border_radius=10
         )
-        
-        # Draw UI
         page.add(hello_box)
         page.update()
 
@@ -46,8 +77,8 @@ def main(page: ft.Page):
         on_click=switch_ui
     )
 
-    # Adds button to the page
-    page.add(login_button)
+    # Adds buttons to the page
+    page.add(name_input, login_button)
 
 # RUN THE APP
 ft.app(main)
